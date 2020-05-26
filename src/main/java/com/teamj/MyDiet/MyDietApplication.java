417acea1ffdc6bcc4ad9ed42765a6fce07c882c1
djotihamNABAGOU <djotiham.nabagou@etu.univ-cotedazur.fr>
2020-05-26 09:11:36 +0200

    initializing back

---
+++ src/main/java/com/teamj/MyDiet/MyDietApplication.java
@@ -0,0 +1,13 @@
+package com.teamj.MyDiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamj/MyDiet/MyDietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamj.MyDiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
