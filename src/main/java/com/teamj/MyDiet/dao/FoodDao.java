c13aa2498c6ff4cc18eb79e20cc125d8dbba9b84
FlorianAinadou <florian.ainadou97@gmail.com>
2020-05-27 15:41:21 +0200

    Adding Somme put and delete methods to food api #1

--- src/main/java/com/teamj/MyDiet/dao/FoodDao.java
+++ src/main/java/com/teamj/MyDiet/dao/FoodDao.java
@@ -3,10 +3,22 @@ package com.teamj.MyDiet.dao;
 import com.teamj.MyDiet.model.Food;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface FoodDao {
-    int insertFood(Food food);
+    int insertFood(UUID id,Food food);
 
+    default int insertFood(Food food){
+        UUID id = UUID.randomUUID();
+        return  insertFood(id, food);
+    }
 
     List<Food> selectAllFood();
+
+    Optional<Food> selectFoodById(UUID id);
+
+    int deleteFoodById(UUID id);
+
+    int updateFoodByName(UUID id, Food food);
 }
