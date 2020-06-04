d698739132b0dc9ff780734540a1ee3c5c1329dc
FlorianAinadou <florian.ainadou97@gmail.com>
2020-06-04 21:33:46 +0200

    Adding method make food add by User

--- src/main/java/com/teamj/MyDiet/controller/FoodController.java
+++ src/main/java/com/teamj/MyDiet/controller/FoodController.java
@@ -4,8 +4,11 @@ package com.teamj.MyDiet.controller;
 import com.teamj.MyDiet.dao.FoodDao;
 import com.teamj.MyDiet.model.Food;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,8 +23,20 @@ public class FoodController {
     }
 
     @PostMapping
-    public void addFood(@RequestBody Food food){
-        foodDao.save(food);
+    public ResponseEntity<Void> addFood(@RequestBody Food food){
+        Food foodToSave= foodDao.save(food);
+
+        if(foodToSave == null){
+            return ResponseEntity.noContent().build();
+        }
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(food.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping(path = "all")
