49989cb1781a36437d9328d6874fde17ff515a79
Kirabium <virgile.fantauzzi@hotmail.fr>
2020-06-05 08:58:39 +0200

    recommandation post fix #8

--- src/main/java/com/teamj/MyDiet/controller/RecommendationController.java
+++ src/main/java/com/teamj/MyDiet/controller/RecommendationController.java
@@ -1,6 +1,7 @@
 package com.teamj.MyDiet.controller;
 
 import com.teamj.MyDiet.dao.RecoDao;
+import com.teamj.MyDiet.model.DailyReport;
 import com.teamj.MyDiet.model.Recommendation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,18 +23,18 @@ public class RecommendationController {
     }
 
     @PostMapping(value = "api/v1/recommendation")
-    public ResponseEntity<Void> addRecommendation(@PathVariable int idUser, @RequestBody Recommendation recommendation){
+    public ResponseEntity<Void> addRecommendation(@RequestBody Recommendation recommendation){
 
         Recommendation recommendation1 = recoDao.save(recommendation);
 
-        if(recommendation1 == null){
+        if(recommendation == null){
             return ResponseEntity.noContent().build();
         }
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
-                .buildAndExpand(recommendation.getId())
+                .buildAndExpand(recommendation1.getId())
                 .toUri();
 
         return ResponseEntity.created(location).build();
