bdb0d146d6f499218ccd6804a3eca0f1e8990870
djotihamNABAGOU <djotiham.nabagou@etu.univ-cotedazur.fr>
2020-06-13 23:33:56 +0200

    data.sql updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,11 +6,11 @@ INSERT INTO daily_report VALUES(400, 2500, 100, 24 , 120, NOW() - INTERVAL 9 DAY
 
 INSERT INTO user VALUES(0,
 'https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/54255583_1276949652454861_874443465881026560_n.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_ohc=88gfQTlQJV0AX__vKxd&_nc_ht=scontent-cdg2-1.xx&oh=869df4abffa857bfe315f77dbf1e4d54&oe=5EFE98AE','No description',
-'florian@mydiet.fr', 1,false, 'Florian', 'patatra');
+'florian@mydiet.fr', null,false, 'Florian', 'patatra');
 
-INSERT INTO user VALUES(1, 'https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/69482110_2450338508361582_8741807199682035712_o.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=qIebk6N6kS8AX-XcLRV&_nc_ht=scontent-cdg2-1.xx&oh=0db7011ea8c36368edb40d67a9a05488&oe=5EFCFD2E','No description',
+INSERT INTO user VALUES(1, 'https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/69482110_2450338508361582_8741807199682035712_o.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=qIebk6N6kS8AX-XcLRV&_nc_ht=scontent-cdg2-1.xx&oh=0db7011ea8c36368edb40d67a9a05488&oe=5EFCFD2E','Coach nutritionnel - Expert sportif - Agréé',
 'virgile@mydiet.fr',null ,true, 'Virgile', 'patatra');
 
 
-INSERT INTO food VALUES(90020, 10, 1, 10, 10,'viande', 10);
-INSERT INTO recommendation VALUES(0, 'Fait du sport', 1, 0);
+//INSERT INTO food VALUES(90020, 10, 1, 10, 10,'viande', 10);
+//INSERT INTO recommendation VALUES(0, 'Fait du sport', 1, 0);
